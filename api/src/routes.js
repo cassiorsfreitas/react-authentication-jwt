5a7b239913487ee361f3641863eaa67d1277868e
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2021-04-30 11:47:48 +0100

    created api folder and added express server on port 333

---
+++ api/src/routes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = require('./auth');
+
+const router = express.Router();
+
+router.post('/authenticate', (req, res) => {
+  const user = {
+    id: 1,
+    name: 'Cássio Freitas',
+    website: 'https://github.com/cassiorsfreitas',
+  };
+
+  return res.json({
+    user,
+    token: jwt.sign(user, 'PRIVATEKEY'),
+  });
+});
+
+/**
+ * Private route
+ */
+router.use(authMiddleware);
+
+router.get('/users', async (req, res) => {
+  return res.json([
+    {
+      id: 1,
+      name: 'Cássio Freitas',
+      website: 'https://github.com/cassiorsfreitas',
+    },
+    {
+      id: 2,
+      name: 'Darth Vader',
+      website: 'https://en.wikipedia.org/wiki/Darth_Vader',
+    },
+    {
+      id: 3,
+      name: 'Oliver Tsubasa',
+      website: 'https://en.wikipedia.org/wiki/Captain_Tsubasam',
+    },
+  ]);
+});
+
+module.exports = router;
