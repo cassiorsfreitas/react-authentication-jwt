5dcec96240b6f9cda5ce545dbe3af19a71786dfe
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2021-05-04 17:38:52 +0100

    added custom hooks useAuth

--- app/src/Context/AuthContext.js
+++ app/src/Context/AuthContext.js
@@ -1,43 +1,12 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext } from 'react';
 
-import api from '../api';
-import history from '../history';
+import useAuth from './hooks/useAuth';
 
 const Context = createContext();
 
 // eslint-disable-next-line react/prop-types
 function AuthProvider({ children }) {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-
-    if (token) {
-      api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`;
-      setAuthenticated(true);
-    }
-
-    setLoading(false);
-  }, []);
-
-  async function handleLogin() {
-    const {
-      data: { token },
-    } = await api.post('/authenticate');
-
-    localStorage.setItem('token', JSON.stringify(token));
-    api.defaults.headers.Authorization = `Bearer ${token}`;
-    setAuthenticated(true);
-    history.push('/users');
-  }
-
-  function handleLogout() {
-    setAuthenticated(false);
-    localStorage.removeItem('token');
-    api.defaults.headers.Authorization = undefined;
-    history.push('/login');
-  }
+  const { loading, authenticated, handleLogin, handleLogout } = useAuth();
 
   return <Context.Provider value={{ loading, authenticated, handleLogin, handleLogout }}>{children}</Context.Provider>;
 }
